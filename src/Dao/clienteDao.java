f09769a206e0b8fedff8b30587a7415c6bc3cbfe
Gabriel Gomes <134211799+Eiigabs@users.noreply.github.com>
2023-06-07 13:44:52 -0300

    Update clienteDao.java

--- src/Dao/clienteDao.java
+++ src/Dao/clienteDao.java
@@ -1,5 +1,4 @@
 package Dao;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -7,7 +6,7 @@ import Model.Cliente;
 import Util.Conexao;
 
 public class clienteDao {
-
+	
 	public boolean InserirCliente(Cliente c){
 		Conexao con = null;
 		try{
@@ -28,7 +27,7 @@ public class clienteDao {
 		
 	}
 	
-	public ArrayList<Cliente> ArrayListaCliente(){
+	public ArrayList<Cliente> ArrayAlterarCliente(){
 		Conexao con = null;
 		try {
 			con = new Conexao();
@@ -54,36 +53,77 @@ public class clienteDao {
 			return null;
 		}
 	}
-	
-	 public boolean verificarLogin(String nomeUsuario, String senha) {
-		 Conexao con = null;
-		 try {
-			 con = new Conexao();
-	           
-	            // Preparar a consulta SQL
-	        	 
-	            String consulta = "SELECT COUNT(*) FROM cliente WHERE cpf = '" + nomeUsuario + "' AND senha = '" + senha + "'";
-	            
-	            // Executar a consulta
-	            
-	            ResultSet rs = con.executeQuery(consulta);
 
-	            // Verificar se o usuário existe no banco de dados
-	            rs.next();
-	            int count = rs.getInt(1);
+	public boolean Logar(Login c){
+		Conexao con = null;
+		String user = ("SELECT * FROM login  WHERE login = '"+usuario+"' AND senha = '"+senha+"' ");
+		try {
+			java.sql.Statement test = con().createStatement();
+			test.executeQuery(user);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("Erro na função logar DAO" + e);
+			return false;
+		}
 
-	          
-	            // Retornar true se o usuário existe e a senha está correta
-	            return count == 1;
-	        } catch (SQLException e) {
-	        	System.out.println("Erro na funcao verificarLogin DAO");
-	        }
+	}
+	
 
-	        // Em caso de erro ou se o usuário não existe, retornar false
-	        return false;
-	    }
+	public boolean AlterarCliente(Cliente c){
+		conexao con = null;
+		try{
+		con = new Conexao();
+		con.executeUpdate("UPDATE cliente SET"
+		+ " nome= '" + cliente.getNome() + "' , "
+		+ " dt_name= '" + cliente.getDt_nascimento()+"', " 
+		+ " cpf= '" + cliente.getCpf()+"' "
+		+ " WHERE id_cliente = " + cliente.getId_Cliente());
+		return true;
+		}catch(SQLExeception e){
+		system.out.println("ERRO");
+		return false;	
+		}
+	}
+
+	public Cliente ListarClienteID(int id){
+		conexao con = null;
+		try{
+		con = new Conexao();
+		ResultSet result = con.executeQuery("SELECT * FROM cliente WHERE id_cliente = " + id);  	
+		result.next();
+		Cliente cliente = Cliente();
+		cliente.setId_cliente(result.getInt("id_cliente"));
+		cliente.setNome(result.getString("nome"));
+		cliente.setCpf(result.getString("cpf"));
+		cliente.setData(result.getString("data_nascimento"));
+		cliente.setTelefone(result.getString("telefone"));
+		cliente.setEmail(result.getString("email"));
+		cliente.setGenero(result.getString("genero"));
+	
+			return cliente;
+		}catch(SQLExeception e){
+		system.out.println("ERRO" + e);
+		
+		}
+	}	
+
+	public boolean DeletarCliente(int id){
+		conexao con = null;
+		try{
+		con = new Conexao();
+		con.executeUpdate("DELETE FROM cliente WHERE id_cliente = " + id);
+		return true;
+		}catch(SQLExeception e){
+		system.out.println("ERRO");
+		return false;	
+		}
 	
 	
-	
+	}
+
+
+
+
+
 
 }
