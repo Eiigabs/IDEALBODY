404e5c6fd35242872f4b7d4528a65d159ead3168
Gabriel Gomes <134211799+Eiigabs@users.noreply.github.com>
2023-06-08 10:13:49 -0300

    Update Cliente.java

--- src/Model/Cliente.java
+++ src/Model/Cliente.java
@@ -2,6 +2,7 @@ package Model;
 public class Cliente{
 	
 	private int id_cliente;
+	private int hid_cliente;
 	private String nome;
 	private String cpf;
 	private String data;
@@ -15,6 +16,12 @@ public class Cliente{
 	}
 	public void setId_cliente(int id_cliente) {
 		this.id_cliente = id_cliente;
+	}
+	public int getHid_cliente() {
+		return hid_cliente;
+	}
+	public void setHid_cliente(int id_cliente) {
+		this.hid_cliente = id_cliente;
 	}
 	public String getNome() {
 		return nome;
